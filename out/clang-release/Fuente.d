92c109cea6317262053e3cff37c9025a623cd724
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-14 00:17:16 +0100

    Añadido gitignore al proyecto

--- out/clang-release/Fuente.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//Fuente.o: Fuente.cc \
-  C:/Users/Edwin/Desktop/omnet/omnetpp-5.2/include\omnetpp.h paquete_m.h
-
-C:/Users/Edwin/Desktop/omnet/omnetpp-5.2/include\omnetpp.h:
-
-paquete_m.h:
--- out/clang-release/Node.d
+++
@@ -1,4 +0,0 @@
-out/clang-release//Node.o: Node.cc \
-  C:/Users/Edwin/Desktop/omnet/omnetpp-5.2/include\omnetpp.h
-
-C:/Users/Edwin/Desktop/omnet/omnetpp-5.2/include\omnetpp.h:
