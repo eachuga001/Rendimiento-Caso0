75543d31fb4e9b391de72b4f873c566f60294454
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-14 00:24:53 +0100

    Generalizado proceso para n nodos, con distinto número de entradas, salidas y probabilidades de elección de puerto de salida

--- Node.cc
+++ Node.cc
@@ -8,25 +8,25 @@ class Node : public cSimpleModule
 {
   protected:
     virtual void forwardMessage(cMessage *msg);
-    //virtual void initialize() override;
+    virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-
-    int setProbability(int numNode,cMessage *msg);
-    int getRoute(double p);
+    virtual int getOutPort();
+  private:
+    std::vector<int> probabilities;
 };
 
 Define_Module(Node);
 
-/*void Txc10::initialize()
+void Node::initialize()
 {
-    if (getIndex() == 0) {
-        // Boot the process scheduling the initial message as a self-message.
-        char msgname[20];
-        sprintf(msgname, "tic-%d", getIndex());
-        cMessage *msg = new cMessage(msgname);
-        scheduleAt(0.0, msg);
-    }
-}*/
+    std::vector<int> distances; // holds result
+
+    //Leer las probabilidades de salida por cada puerta de salida.
+    const char *probs = par("probabilities");
+    cStringTokenizer tokenizer(probs);
+    while (tokenizer.hasMoreTokens())
+        probabilities.push_back(atoi(tokenizer.nextToken()));
+}
 
 void Node::handleMessage(cMessage *msg)
 {
@@ -47,51 +47,20 @@ void Node::forwardMessage(cMessage *msg)
     //int k = intuniform(0, n-1);
 
     //int port = getRoute(0.25);
-    int port = setProbability(getIndex(),msg);
-
-    EV<< "Nodo-"<<getIndex() << " Enviando por el puerto-"<<port;
+    int port = getOutPort();
+    send(msg,"out",port);
+    //EV<< "Nodo-"<<getIndex() << " Enviando por el puerto-"<<port;
     //EV << "Forwarding message " << port << " on port out[" << port << "]\n";
 //    send(msg, "out",port);
 }
 
-int Node::setProbability(int numNode,cMessage *msg){
-    int port;
-    switch(numNode){
-    case 0:
-        port = getRoute(0.25);
-        send(msg, "out",port);
-        break;
-    case 1:
-        port = getRoute(0.33);
-        send(msg, "out",port);
-        break;
-    case 2:
-        port = 0;
-        send(msg, "out",port);
-        break;
-    case 3:
-        port=0;
-        send(msg, "out",port);
-        break;
-    case 4:
-        port = getRoute(0.5);
-        send(msg, "out",port);
-        break;
+int Node::getOutPort(){
+    int port = 0;
+    int rand = uniform (0,1);
+    while (rand > probabilities[port])
+    {
+        port++;
+        rand = rand - probabilities[port];
     }
     return port;
-}
-
-int Node::getRoute(double p){
-    //Mayor probabilidad para el puerto 1
-    int outPort;
-    double random;
-    random = uniform(0,1);
-    //EV << "randomNum = " << random;
-    if(random<p)
-        outPort = 0;
-    else
-        outPort=1;
-
-    //EV << " The port is: " << outPort;
-    return outPort;
 }
